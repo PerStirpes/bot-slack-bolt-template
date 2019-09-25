feb42dae4e5532e7a24cd351e364ce2a10d0dc3c
Matt Barraza-Carl <mbarrazacarl@launchdarkly.com>
2019-09-25 13:22:42 -0700

    working examples

--- src/app.ts
+++ src/app.ts
@@ -8,19 +8,28 @@ import {
   directMention,
   BlockButtonAction,
   Middleware,
+  SayFn,
+  SlackEventMiddlewareArgs,
+  MessageEvent,
 } from '@slack/bolt';
-import { ErrorCode } from '@slack/web-api';
-// WebAPICallResult
-import * as WebApi from 'seratch-slack-types/web-api';
-
+import { ErrorCode, ChatGetPermalinkArguments, WebAPICallResult } from '@slack/web-api';
+// addUser, getUser,
+import { getMessages, getChannel, getMe, setChannel, setMe } from './store';
+import { copy, getUrlWithParams } from './helpers';
+import { messages } from './messages';
+import { StringIndexed } from '@slack/bolt/dist/types/helpers';
 const app: App = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+  authorize: () => {
+    return Promise.resolve({
+      botId: 'BNP5WM5LP',
+      botToken: process.env.SLACK_BOT_TOKEN,
+      userToken: process.env.SLACK_USER_TOKEN,
+    });
+  },
+
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   logLevel: LogLevel.DEBUG,
 });
-
-// import * as Backend from 'seratch-slack-typesapp/';
-// import { Action } from 'seratch-slack-types/web-api/ChannelsHistoryResponse';
 
 type SlackError = {
   code: string;
@@ -169,6 +178,47 @@ app.action('escalate_button', async ({ ack, action, context }) => {
   } catch (error) {
     errorDescription(error);
   }
+});
+
+let incidentOpenMessage: any = undefined;
+
+app.action('incident_ack', async ({ action, body, ack, say, context }) => {
+  console.log(action, body);
+  const button: BlockButtonAction = body as BlockButtonAction;
+  const { message } = button;
+  ack();
+  if (!message) {
+    return;
+  }
+  if (!incidentOpenMessage) {
+    say('This incident was already acknowledged.');
+    return;
+  }
+
+  const blocksIncidentAck = [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*Incidents*\n\nThe incident was resolved by <@${body.user.id}>. :tada:`,
+      },
+    },
+  ];
+  await app.client.chat.update({
+    text: 'Incident ackknowledged',
+    ts: message.ts,
+    channel: body.channel.id,
+    blocks: blocksIncidentAck,
+    token: context.botToken,
+  });
+
+  await app.client.reactions.add({
+    name: 'white_check_mark',
+    channel: body.channel.id,
+    timestamp: incidentOpenMessage,
+    token: context.botToken,
+  });
+  incidentOpenMessage = undefined;
 });
 
 /*
@@ -397,6 +447,53 @@ app.message(':red_circle:', async ({ message, say }) => {
   });
 });
 
+app.message(({ message, say }) => {
+  console.log('TCL: message within text includes red_circle', message);
+  console.log(message);
+
+  if (message.subtype !== undefined) {
+    return;
+  }
+
+  if (message.text && message.text.includes('red_circle')) {
+    if (incidentOpenMessage) {
+      say('An incident is already open. Please resolve the previous incident.');
+      return;
+    }
+    incidentOpenMessage = message.ts;
+
+    say({
+      text: 'Ackknowledgement',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text:
+              '*Incidents*\n\nAn incident has been created based on your message. This app is here to help you organize the communications with all the right people. :sweat_smile:',
+          },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              action_id: 'incident_ack',
+              text: {
+                type: 'plain_text',
+                text: 'Acknowledge',
+              },
+              value: 'incident_ack',
+            },
+          ],
+        },
+      ],
+    });
+  } else {
+    say(`Hey <@${message.user}>`);
+  }
+});
+
 /*
 ###############################################################
 
@@ -425,11 +522,70 @@ app.command(
 
 ###############################################################
 */
+interface UsersInfoResponse {
+  ok?: boolean;
+  user?: User;
+  error?: string;
+  needed?: string;
+  provided?: string;
+}
+
+interface User {
+  id?: string;
+  team_id?: string;
+  name?: string;
+  deleted?: boolean;
+  color?: string;
+  real_name?: string;
+  tz?: string;
+  tz_label?: string;
+  tz_offset?: number;
+  profile?: Profile;
+  is_admin?: boolean;
+  is_owner?: boolean;
+  is_primary_owner?: boolean;
+  is_restricted?: boolean;
+  is_ultra_restricted?: boolean;
+  is_bot?: boolean;
+  is_app_user?: boolean;
+  updated?: number;
+  has_2fa?: boolean;
+}
+
+interface Profile {
+  title?: string;
+  phone?: string;
+  skype?: string;
+  real_name?: string;
+  real_name_normalized?: string;
+  display_name?: string;
+  display_name_normalized?: string;
+  status_text?: string;
+  status_emoji?: string;
+  status_expiration?: number;
+  avatar_hash?: string;
+  bot_id?: string;
+  api_app_id?: string;
+  always_active?: boolean;
+  image_original?: string;
+  first_name?: string;
+  last_name?: string;
+  image_24?: string;
+  image_32?: string;
+  image_48?: string;
+  image_72?: string;
+  image_192?: string;
+  image_512?: string;
+  image_1024?: string;
+  status_text_canonical?: string;
+  team?: string;
+  is_custom_image?: boolean;
+}
 
 app.event('app_mention', async ({ event, say, context }) => {
   console.log('event: ', event.user);
   try {
-    const res: WebApi.UsersInfoResponse = await app.client.users.info({
+    const res: UsersInfoResponse = await app.client.users.info({
       token: context.botToken,
       user: event.user,
     });
@@ -458,25 +614,204 @@ app.event('member_joined_channel', ({ event, say }) => {
 });
 app.event('member_left_channel', ({ say }) => say(randomLeaveReply()));
 
-// app.event("reaction_added", async ({ event, say }) => {
+app.event('member_joined_channel', async ({ context, event, say }) => {
+  const channel = getChannel();
+  const user = event.user;
+
+  // check if our Bot user itself is joining the channel
+  if (user === getMe() && channel) {
+    const message = copy(messages.welcome_channel);
+    // fill in placeholder values with channel info
+    message.blocks[0].text.text = message.blocks[0].text.text
+      .replace('{{channelName}}', channel.name)
+      .replace('{{channelId}}', channel.id);
+    say(message);
+  }
+});
+interface ReactionAddedEvent {
+  type: 'reaction_added';
+  user: string;
+  reaction: string;
+  item_user: string;
+  item: Item;
+  event_ts: string;
+}
+interface ReactionAddedPayload {
+  token?: string;
+  team_id?: string;
+  api_app_id?: string;
+  event?: Event;
+  type?: string;
+  authed_users?: string[];
+  event_id?: string;
+  event_time?: number;
+  channel?: string;
+}
+interface Event {
+  type?: string;
+  user?: string;
+  reaction?: string;
+  item_user?: string;
+  item?: Item;
+  event_ts?: string;
+}
+interface Item {
+  type?: string;
+  channel?: string;
+  ts?: string;
+  file?: string;
+  file_comment?: string;
+}
+interface UsersInfoResponse {
+  ok?: boolean;
+  user?: User;
+  error?: string;
+  needed?: string;
+  provided?: string;
+}
+
+interface User {
+  id?: string;
+  team_id?: string;
+  name?: string;
+  deleted?: boolean;
+  color?: string;
+  real_name?: string;
+  tz?: string;
+  tz_label?: string;
+  tz_offset?: number;
+  profile?: Profile;
+  is_admin?: boolean;
+  is_owner?: boolean;
+  is_primary_owner?: boolean;
+  is_restricted?: boolean;
+  is_ultra_restricted?: boolean;
+  is_bot?: boolean;
+  is_app_user?: boolean;
+  updated?: number;
+  has_2fa?: boolean;
+}
+
+interface Profile {
+  title?: string;
+  phone?: string;
+  skype?: string;
+  real_name?: string;
+  real_name_normalized?: string;
+  display_name?: string;
+  display_name_normalized?: string;
+  status_text?: string;
+  status_emoji?: string;
+  status_expiration?: number;
+  avatar_hash?: string;
+  bot_id?: string;
+  api_app_id?: string;
+  always_active?: boolean;
+  image_original?: string;
+  first_name?: string;
+  last_name?: string;
+  image_24?: string;
+  image_32?: string;
+  image_48?: string;
+  image_72?: string;
+  image_192?: string;
+  image_512?: string;
+  image_1024?: string;
+  status_text_canonical?: string;
+  team?: string;
+  is_custom_image?: boolean;
+}
+
+app.event('reaction_added', async ({ event, context }) => {
+  // only react to ⚡ (:zap:) emoji
+  //
+  // const events: ReactionAddedEvent = event as ReactionAddedPayload;
+  // let event: ReactionAddedEvent;'
+
+  if (event.reaction === 'zap') {
+    const { channel, ts } = event.item as MessageEvent;
+
+    // get a permalink for this message
+    const permalink = await app.client.chat.getPermalink({
+      token: context.botToken,
+      message_ts: ts,
+      channel: channel,
+    });
+
+    // get user info of user who reacted to this message
+    const user: any = await app.client.users.info({
+      token: context.botToken,
+      user: event.user,
+    });
+
+    //
+
+    const name = '<@' + user.user.id + '>';
+    const channelGot = getChannel();
+
+    // post this message to the configured channel
+    await app.client.chat.postMessage({
+      token: context.botToken,
+      channel: channelGot && channelGot.id,
+      text: name + ' wants you to see this message: ' + permalink.permalink,
+      unfurl_links: true,
+      unfurl_media: true,
+    });
+
+    // formatting the user's name to mention that user in the message (see: https://api.slack.com/messaging/composing/formatting)
+  }
+});
+
+// app.event('reaction_added', async ({ event, say }) => {
+//   console.log(event);
+//   const said = say as SayFn;
+
+//   say({ text: `:${event.reaction}:` });
+// });
+
+// app.event('reaction_added', async ({ event, say }) => {
 //   // only react to a certain emoji. :frowning: for example
-//   if (event.reaction === "frowning") {
+//   if (event.reaction === 'frowning') {
 //     // const channelId = event.item.channel;
 //     // const ts = event.item.ts;
 //     const message_blocks = [
 //       {
-//         type: "section",
+//         type: 'section',
 //         text: {
-//           type: "plain_text",
-//           text: "This is a plain text section block.",
-//           emoji: true
-//         }
-//       }
+//           type: 'plain_text',
+//           text: 'This is a plain text section block.',
+//           emoji: true,
+//         },
+//       },
 //     ];
 //     // Respond to the message with a button
 //     say({
-//       text: "This is a plain text section block.",
-//       blocks: message_blocks
+//       text: 'This is a plain text section block.',
+//       blocks: message_blocks,
+//     });
+//   }
+// });
+// app.event('reaction_added', ({ event, say }) => {
+//   if (event.reaction === 'calendar') {
+//     say({
+//       blocks: [
+//         {
+//           type: 'section',
+//           text: {
+//             type: 'mrkdwn',
+//             text: 'Pick a date for me to remind you',
+//           },
+//           accessory: {
+//             type: 'datepicker',
+//             action_id: 'datepicker_remind',
+//             initial_date: '2019-04-28',
+//             placeholder: {
+//               type: 'plain_text',
+//               text: 'Select a date',
+//             },
+//           },
+//         },
+//       ],
 //     });
 //   }
 // });
