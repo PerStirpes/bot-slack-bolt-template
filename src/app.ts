5ca7443e046ae6958eb8c00d8ce7c9182a5750d8
MC <19398470+PerStirpes@users.noreply.github.com>
2019-09-25 23:12:31 -0700

    Pulls out error function

--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,84 @@
-import { App, LogLevel } from '@slack/bolt';
-import * as WebApi from 'seratch-slack-types/web-api';
-import { ErrorCode } from '@slack/web-api';
+import { App, LogLevel } from "@slack/bolt";
+import * as WebApi from "seratch-slack-types/web-api";
+import { errorDescription } from "./utils";
+import { asCodedError } from "@slack/bolt/dist/errors";
 
 const app: App = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+  authorize: () => {
+    return Promise.resolve({
+      botId: "BNP5WM5LP",
+      botToken: process.env.SLACK_BOT_TOKEN,
+      userToken: process.env.SLACK_USER_TOKEN
+    });
+  },
+
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  logLevel: LogLevel.DEBUG,
+  logLevel: LogLevel.DEBUG
 });
 
-type SlackError = {
-  code: string;
-  data: object;
-};
-
-function errorDescription(error: SlackError): void {
-  if (error.code === ErrorCode.RequestError) {
-    // Some other error, oh no!
-    console.log('ErrorCode.RequestError ', error.data);
-  } else if (error.code === ErrorCode.PlatformError) {
-    console.log('ErrorCode.PlatformError', error.data);
-  } else if (error.code === ErrorCode.HTTPError) {
-    // Some other error, oh no!
-    console.log('ErrorCode.HTTPError ', error.data);
-  } else {
-    console.error('Well that was unexpected! ', error);
-  }
-}
-
 /*
+
 ########################### MESSAGES ########################### 
 
 */
 
-// const response = await web.users.info({ user: "..." });
-app.message('happy', async ({ message, context }) => {
+app.message("happy", async ({ message, context }) => {
   try {
     const result = await app.client.reactions.add({
       token: context.botToken,
-      name: 'grinning',
+      name: "grinning",
       channel: message.channel,
-      timestamp: message.ts,
+      timestamp: message.ts
     });
     const response = await app.client.reactions.add({
       token: context.botToken,
-      name: 'star',
+      name: "star",
       channel: message.channel,
-      timestamp: message.ts,
+      timestamp: message.ts
     });
-    console.log('reactions.add result & response:', result, response);
+    console.log("reactions.add result & response:", result, response);
   } catch (error) {
     errorDescription(error);
   }
 });
 
-app.message('42', ({ message, context }) => {
+// Threads a message
+app.message("42", ({ message, context }): void => {
   // use chat.postMessage over say method
   try {
     const response = app.client.chat.postMessage({
       token: context.botToken,
       channel: message.channel,
-      text: 'The answer to life, the universe and everything',
-      thread_ts: message.ts,
+      text: "The answer to life, the universe and everything",
+      thread_ts: message.ts
     });
-    console.log('response from postMessage', response);
-  } catch (reason) {
-    console.error(`Failed because ${reason}`);
+    console.log("response from postMessage", response);
+  } catch (error) {
+    errorDescription(error);
   }
 });
 
-app.message('hello', ({ message, say }) => {
+// sends a button
+app.message("hello", ({ message, say }) => {
   say({
     text: `Ewok is a dog!`,
     blocks: [
       {
-        type: 'section',
+        type: "section",
         text: {
-          type: 'mrkdwn',
-          text: `Hey there <@${message.user}>!`,
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`
         },
         accessory: {
-          type: 'button',
+          type: "button",
           text: {
-            type: 'plain_text',
-            text: 'Click Me',
+            type: "plain_text",
+            text: "Click Me"
           },
-          action_id: 'button_click',
-        },
-      },
-    ],
+          action_id: "button_click"
+        }
+      }
+    ]
   });
 });
 
@@ -96,65 +87,80 @@ app.message('hello', ({ message, say }) => {
 ########################### ACTIONS ########################### 
 */
 
-app.action('button_click', ({ action, ack, body, say }) => {
+app.action("button_click", ({ action, ack, body, say }) => {
   ack();
   say(`<@${body.user.id}> clicked the button  ${action.type}`);
 });
 
 /*
+
 ########################### COMMANDS ########################### 
 
 */
 
-app.command('/echo', async ({ command, ack, say }) => {
+app.command("/echo", async ({ command, ack, say }) => {
   // Acknowledge command request
   ack();
   say(`You said "${command.text}"`);
 });
 
 /*
+
 ########################### EVENTS ########################### 
+
 */
 
-app.event('app_mention', async ({ event, say, context }) => {
-  console.log('event: ', event.user);
+app.event("app_mention", async ({ event, say, context }) => {
+  console.log("event: ", event.user);
   try {
     const res: WebApi.UsersInfoResponse = await app.client.users.info({
       token: context.botToken,
-      user: event.user,
+      user: event.user
     });
 
     if (res.ok) {
       if (res.user) {
         say({
-          text: `Hi! <@${res.user.name}>`,
+          text: `Hi! <@${res.user.name}>`
         });
       }
     }
   } catch (reason) {
-    console.error(`Failed because ${reason}`);
+    errorDescription(reason);
   }
 });
 
 /*
+
 ########################### Start App ########################### 
+
 */
 
-(async (PORT = 3000) => {
-  try {
-    await app.start(process.env.PORT || PORT);
-    console.log('TCL: process.env.PORT', process.env.PORT);
-  } catch (error) {
-    throw error;
-  }
+if (process.env.NODE_ENV !== "production") {
+  (async (PORT = 3000) => {
+    try {
+      await app.start(process.env.PORT || PORT);
+    } catch (error) {
+      throw error;
+    }
 
-  console.log('Bolt is running');
-})();
+    console.log(
+      `> Workplace Ops Bolt is running on PORT ${PORT} in ${process.env.NODE_ENV}`
+    );
+  })();
+}
 
-app.error((error) => {
-  console.error(error);
+app.error(err => {
+  console.error("As Codeded Error", asCodedError(err));
 });
 
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+process.on("uncaughtException", function(err) {
+  console.error(err.stack);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", function(reason, p) {
+  console.error("Unhandled rejection", reason);
+});
 
 export default app;
